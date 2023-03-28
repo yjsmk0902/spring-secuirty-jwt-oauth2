0f0fe9714ef6c64ae81bf1c18ef7affb38884d9d
Luke_YS <yjsmk0902@gmail.com>
2023-03-28 10:37:07 +0900

    CustomOAuth2User 추가
    추가 정보를 받아오기 위해 DefaultOAuth2User를 상속받아 커스텀

---
+++ src/main/java/springjwtoauth2/practice/web/oauth2/CustomOAuth2User.java
@@ -0,0 +1,24 @@
+package springjwtoauth2.practice.web.oauth2;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import springjwtoauth2.practice.domain.Role;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User extends DefaultOAuth2User {
+
+    //Resource Server에서 제공하지 않는 추가 정보(여기에서는 emil)들을
+    //내 서비스에 가지고 있기 위해서 DefaultOAuth2User를 직접 사용하지 않고 커스텀함
+
+    private String email;
+    private Role role;
+
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey,
+                            String email, Role role) {
+        super(authorities, attributes, nameAttributeKey);
+        this.email = email;
+        this.role = role;
+    }
+}
